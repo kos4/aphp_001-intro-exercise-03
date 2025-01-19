55217b0de4484293c5e606e4d0261c16d9632da8
kos4 <kos4.kks@gmail.com>
2025-01-19 16:30:40 +0500

    Задание 3. Работа с автозагрузчиком

--- autoloader.php
+++ autoloader.php
@@ -1,4 +1,8 @@
 <?php
-spl_autoload_register(static function ($class_name) {
-  require_once __DIR__ . '/' . str_replace('\\', '/', $class_name . '.php');
+spl_autoload_register(static function ($className) {
+  $path = __DIR__ . '/' . str_replace('\\', DIRECTORY_SEPARATOR, $className . '.php');
+
+  if (is_file($path)) {
+    require_once $path;
+  }
 });
